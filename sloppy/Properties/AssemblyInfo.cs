57660eab9fb04c8e17531788424da5cf3c125c37
n416 <nakamura.shingo@gmail.com>
2016-09-24 23:33:16 +0900

    アセンブリ情報を付与しておく

--- sloppy/Properties/AssemblyInfo.cs
+++ sloppy/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
-[assembly: AssemblyTitle("logtran")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("sloppy")]
+[assembly: AssemblyDescription("翻訳したりしなかったり")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("logtran")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("株式会社如月システム")]
+[assembly: AssemblyProduct("sloppy")]
+[assembly: AssemblyCopyright("Copyright ©kisaragi-system  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -34,3 +35,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("ja-JP")]
+
